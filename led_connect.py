0a7efa9678425f77bf3d68d493e223e96ba383a0
Carlos Mandele <104703594+carlosmandele@users.noreply.github.com>
2024-12-20 01:24:08 -0300

    Import modules, create and configure the scope of the program.

--- led_connect.py
+++ led_connect.py
@@ -1,6 +1,26 @@
 # Import libs
 import time
+from machine import*
+import utime
 
 
 time.sleep(0.1)
 print("Hello, Raspberry Pi Pico W!")
+
+
+# Déclaration d'une broche en sortie, à l'aide du constructeur Pin()
+# Setting an output pin, using the Pin() constructor
+led = Pin(3, Pin.OUT)
+
+
+# Allumer la LED
+# Turn on the LED
+led.on()
+utime.sleep(2)
+led.off()
+
+while True:
+  led.off()
+  utime.sleep(1)
+  led.on()
+  utime.sleep(1)
